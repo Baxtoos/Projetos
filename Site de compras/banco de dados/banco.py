9cace0e239ff159da36470e2a7b750d038631f72
Sérgio Bastos <giftc444@gmail.com>
2025-06-20 22:09:12 -0300

    .

--- Site de compras/banco de dados/banco.py
+++ Site de compras/banco de dados/banco.py
@@ -1,4 +1,6 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orn import sessionmaker, declarative_base
 
 db = create_engine("sqlite.///C:\Users\Sérgio\Documents\MeusCodigos\Projetos\Site de compras\banco de dados\banco.py")
-
+Session = sessionmaker(bind=db) 
+session = session()
